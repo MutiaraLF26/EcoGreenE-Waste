959ae07f0586046a05bd476d5c1aa8f9e7e5af73
Wina Agustina <wina.213040093@mail.unpas.ac.id>
2024-01-09 13:36:00 +0700

    Mengubah EditUser

--- src/EcoGreen/Login.java
+++ src/EcoGreen/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package ecogreene.waste;
+package EcoGreen;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -50,7 +50,6 @@ public class Login extends javax.swing.JFrame {
         password = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(360, 650));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setPreferredSize(new java.awt.Dimension(360, 650));
